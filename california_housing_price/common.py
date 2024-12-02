309f79ec9aa231eecb63e71e971a0bcaea427e2b
MahiElamine02 <guend.pro.02@gmail.com>
2024-12-02 10:07:39 +0100

    avancement

--- california_housing_price/common.py
+++ california_housing_price/common.py
@@ -10,8 +10,12 @@ def sets_creation(df,first_threshold, ratio):
     training_set = df[:training_set_treshold]
     test_set = df[training_set_treshold:]
 
+    # Separer le y (variable a predire) des X (variables explicatives)
+    X = test_set.drop(columns=['median_house_value'])
+    Y = test_set['median_house_value']
+
     print("training_set size", len(training_set))
     print("test_set size", len(test_set))
 
-    return training_set, test_set
+    return X, Y
     
